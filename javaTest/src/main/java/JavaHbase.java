e2a3d46db95c56f3091153ea07e9f9cf6d7416b5
zhoujun134 <1161386101@qq.com>
2017-09-09 17:41:14 +0800

    commit test

--- javaTest/src/main/java/JavaHbase.java
+++ javaTest/src/main/java/JavaHbase.java
@@ -4,5 +4,9 @@
 public class JavaHbase {
     public static void main(String[] args){
         System.out.println("Test");
+        System.out.println("Test");
+        System.out.println("Test");
+        System.out.println("Test");
+        System.out.println("Test");
     }
 }
